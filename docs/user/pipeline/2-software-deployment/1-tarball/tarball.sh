ee32df295ce836b60c49de6822f8770012c8f004
Kolen Cheung <christian.kolen@gmail.com>
2023-08-19 02:34:34 +0100

    add 2-software-deployment/

---
+++ docs/user/pipeline/2-software-deployment/1-tarball/tarball.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+# helpers ##############################################################
+
+COLUMNS=72
+
+print_double_line() {
+	eval printf %.0s= '{1..'"${COLUMNS}"\}
+	echo
+}
+
+print_line() {
+	eval printf %.0s- '{1..'"${COLUMNS}"\}
+	echo
+}
+
+########################################################################
+
+print_double_line
+echo "Unpacking environment..."
+tar -xzf pmpm-20230718-Linux-x86_64-OpenMPI.tar.gz -C /tmp
+. /tmp/pmpm-20230718/bin/activate /tmp/pmpm-20230718
+print_line
+echo "Environment is available at:"
+which python
+which mpirun
+
+# print_double_line
+echo "Running TOAST tests..."
+mpirun -n 8 python -c "import toast.tests; toast.tests.run()"
+
+# print_double_line
+echo "Running TOAST benchmarks..."
+mpirun -n 8 toast_benchmark.py
