aba152c2e5d9c73ffc684bb50743fb58c42a9670
Kolen Cheung <christian.kolen@gmail.com>
2023-11-10 19:13:22 +0000

    Bump version: 0.1.2 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ author = "SO:UK Collaborators"
 year = "2023"
 copyright = f"{year}, {author}"
 del year
-release = "0.1.2"
+release = "0.2.0"
 version = ".".join(release.split(".")[:-1])
 extensions = [
     "sphinx.ext.autodoc",
