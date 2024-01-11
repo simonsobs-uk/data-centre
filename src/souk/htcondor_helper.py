f44431e6b7d28fffb8c4a4c570a9b267685dc9c7
Kolen Cheung <christian.kolen@gmail.com>
2024-01-11 20:39:57 +0000

    fix docstring

--- src/souk/htcondor_helper.py
+++ src/souk/htcondor_helper.py
@@ -6,7 +6,8 @@ def get_hostnames() -> list[str]:
 
     Similar to this command:
 
-        run sudo condor_status -format "%s\n" Machine | sort -u
+        run sudo condor_status -format "%s\\n" Machine | sort -u
+
     """
     cmd = ["sudo", "condor_status", "-format", "%s\n", "Machine"]
     result = subprocess.run(cmd, text=True, check=True, capture_output=True)
