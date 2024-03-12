d2f7d957f23a6a4135d05a8b9140a39ead8f76d9
Kolen Cheung <christian.kolen@gmail.com>
2024-03-12 15:35:59 +0000

    add generation

--- src/souk/system/arch_info.py
+++ src/souk/system/arch_info.py
@@ -123,6 +123,10 @@ class System:
     def host_generic_optimization_flags(self) -> str:
         return self.host_generic.optimization_flags("gcc", 12)
 
+    @property
+    def host_generation(self) -> int:
+        return self.host.generation
+
     @cached_property
     def physical_cpu_count(self) -> int:
         return psutil.cpu_count(logical=False)
@@ -163,6 +167,7 @@ class System:
         data: dict[str, Any] = {}
         data["uname"] = self.uname
         data["archspec.cpu.host"] = self.host.to_dict()
+        data["archspec.cpu.host.generation"] = self.host_generation
         data["archspec.cpu.host.optimization_flags.gcc_12"] = (
             self.host_optimization_flags
         )
@@ -240,6 +245,7 @@ class Systems:
             "cpuinfo.brand_raw",
             "archspec.cpu.host.vendor",
             "archspec.cpu.host.name",
+            "archspec.cpu.host.generation",
             "archspec.cpu.host.generic.name",
             "system_cpu_info.Socket(s)",
             "psutil.cpu_count.logical",
