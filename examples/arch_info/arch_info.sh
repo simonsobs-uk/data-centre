55665f18bb87f650595a196d0c8f4d8b0d02f1e0
Kolen Cheung <christian.kolen@gmail.com>
2024-03-12 16:10:06 +0000

    use so-conda-py310-latest instead

--- examples/arch_info/arch_info.sh
+++ examples/arch_info/arch_info.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONDA_PREFIX=/cvmfs/northgrid.gridpp.ac.uk/simonsobservatory/opt/all310-conda-forge
+CONDA_PREFIX=/cvmfs/northgrid.gridpp.ac.uk/simonsobservatory/conda/so-conda-py310-latest
 . "$CONDA_PREFIX/bin/activate" "$CONDA_PREFIX"
 
 ./arch_info.py arch-info "$@"
